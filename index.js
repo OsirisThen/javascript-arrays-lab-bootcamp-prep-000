1f5a269d54e194a22eaa44d5abb838563bcd7c27
Osiris Then <osiriscuevas@hotmail.com>
2018-08-30 01:36:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  name.push()
+  return name;
+}
+
+function destructivelyPrependKitten(name){
+  name.unshift('kitten')
+  return name;
+}
